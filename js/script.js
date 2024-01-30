13675d1f5dc6f0555fce43889646e0929bd3329f
Srivind Viswanathan <71314033+srivind@users.noreply.github.com>
2024-01-30 12:43:30 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+function appLoaded() {
+    AOS.init();
+
+    const typed= document.getElementById('typer'); 
+    new Typewriter(typed, {
+        loop: true,
+        strings: ['Software Engineer', 'Full-Stack Developer','UI Designer'],
+        autoStart: true,
+    });  
+
+    $(document).ready(function(){
+        $('.slick-slider').slick({
+            slidesToShow: 3,
+            infinite:true,
+            autoplay: true,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                  breakpoint: 906, 
+                  settings: {
+                    slidesToShow: 2 
+                  }
+                },
+                {
+                    breakpoint: 550, 
+                    settings: {
+                      slidesToShow: 1 
+                    }
+                }
+            ]
+        });
+    });
+}
